1698eac99cfdd1a972a73f712436b7ed98e46937
marcomag <marcomagnetti@gmail.com>
2015-10-18 19:48:32 +0200

    updated collection fetcher

--- app/scripts/routing.js
+++ app/scripts/routing.js
@@ -10,19 +10,11 @@ var routing = [
     activity:'act-users',
     title:'Users',
     icon:'account-box',
-    menu:'/users/list/sort:3,asc;search:bla+bla+bla'
+    menu:'/users/list/0'
   },
 
+
+  /*404 error*/
   {path:'*', activity:'act-not-found',   title:'404 Not found',    icon:'account-box'                      }
-
-
-  /*
-  {path:'/modules',   activity:'act-modules', title:'Modules',  icon:'home', menu:true},
-  {path:'/',          activity:'act-home',    title:'Home',     icon:'home', menu:true},
-  {path:'/',          activity:'act-home',    title:'Home',      icon:'home', menu:true},
-  /*
-  {activity:'modules',    name:'Modules',   path:'/modules',  icon:'home'        },
-  {activity:'users',      name:'Users',     href:'/users',    icon:'account-box' },
-  {activity:'contact',    name:'Groups',    href:'/contact',  icon:'group-work'  },*/
 
 ];
