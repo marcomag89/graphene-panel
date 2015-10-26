20bc98e8b2c129da6aa9488e20d64d1470b5a7bc
marcomag <marcomagnetti@gmail.com>
2015-10-26 11:40:54 +0100

    fix issue critica auenticazione

--- app/conf/routing.js
+++ app/conf/routing.js
@@ -3,22 +3,15 @@
  */
 
 var routing = [
-  {path:'/',             activity:'act-home'},
-  {path:'/:model/list',  activity:'act-list'},
+  {path:'/',                 activity:'act-home'},
+
+  // [users/list]
+  {path:'/:model/list',      activity:'act-list'},
+
+  // [users/create/0, users/read/ID /users/update/ID]
+  {path:'/:model/:mode/:id', activity:'act-crud'},
 
 
-  //{path:'/users/list/', activity:'act-users', title:'Users', icon:'account-box', menu: '/users/list'},
-
- /*
-  {
-    path      : '/users/:mode/:par',
-    activity  : 'act-crud',
-    title     : 'Users',
-    icon     : 'account-box',
-    menu     : '/users/list/0',
-    settings : {postfix:'users'}
-  },
-*/
   /*404 error*/
   {path:'*', activity:'act-not-found',   title:'404 Not found',    icon:'account-box'                      }
 
