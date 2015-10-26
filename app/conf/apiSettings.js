47b60d09a91c1c17db5aa395d872bee38c36bc42
marcomag <marcomagnetti@gmail.com>
2015-10-26 14:24:39 +0100

    graphene v0.2.3RC

--- app/conf/apiSettings.js
+++ app/conf/apiSettings.js
@@ -1,7 +1,8 @@
 var apiSettings = {
   server : {
-    url    : 'http://localhost/Graphene/api',
-    apiKey : 'd2d3ab2b97aa75874ada4931f4ce8586f084196317425'
+    appName : 'admin-panel',
+    url     : 'http://localhost/Graphene/api',
+    apiKey  : 'd2d3ab2b97aa75874ada4931f4ce8586f084196317425'
   },
   actions : {
     users:{crud:'USERS.$CRUD'}
