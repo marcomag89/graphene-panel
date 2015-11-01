e66cd6898ac181ea5babe82641aa2103518b334a
marcomag <marcomagnetti@gmail.com>
2015-11-01 02:54:42 +0100

    Collection update - Updated graphene collection for data approach

--- app/conf/apiSettings.js
+++ app/conf/apiSettings.js
@@ -4,7 +4,11 @@ var apiSettings = {
     url     : 'http://localhost/Graphene/api',
     apiKey  : 'd2d3ab2b97aa75874ada4931f4ce8586f084196317425'
   },
+
   actions : {
-    users:{crud:'USERS.$CRUD'}
+    users:{
+      crud       : 'USERS.$CRUD',
+      collection : 'USERS.COLLECTION'
+    }
   }
 };
